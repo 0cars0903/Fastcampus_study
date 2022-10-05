c1bd77b9ef1e0b85eb6e01a774215a672a239451
JunHees <thdwnsgml521@gmail.com>
2022-10-05 10:54:23 +0900

    Frame 제작 방법_2

--- CH03_01.py
+++ CH03_01.py
@@ -1,4 +1,8 @@
+from multiprocessing import dummy
 import pandas as pd
 df = pd.DataFrame({'a' : [1,2,3], 'b' : [4,5,6], 'c' : [7,8,9]})
 print(type(df))
 print(df)
+dummy = {'a' : [1,2,3], 'b' : [4,5,6], 'c' : [7,8,9]}
+df2 = pd.DataFrame(dummy)
+print(df2)
